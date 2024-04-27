62ce722889fe0f8198df6a5a5c96ad256e5d9af9
sushruta19 <sensoubhik2001@gmail.com>
2024-04-27 07:31:28 +0530

    Add projects page view Create Project.astro component

---
+++ src/content/projects.js
@@ -0,0 +1,35 @@
+const projects = [
+  {
+    id : 1,
+    name : "Book Store",
+    description: "Full stack MERN project that allows users to display, add, edit, and delete books",
+    github_link: "https://github.com/sushruta19/book-store",
+    live_link: "",
+    imgUrl: ""
+  },
+  {
+    id : 2,
+    name : "Dummy Crypto Coin",
+    description: "A simplified version of bitcoin blockchain implemented from scratch in TypeScript",
+    github_link: "https://github.com/sushruta19/dummycoin",
+    live_link: "",
+    imgUrl: ""
+  },
+  {
+    id : 3,
+    name : "EJS Blog Application",
+    description: "A simple blog website created using EJS templates and Express with MongoDB database integration",
+    github_link: "https://github.com/sushruta19/beginner-blog-application",
+    live_link: "",
+    imgUrl: ""
+  },
+  {
+    id : 4,
+    name : "EasyProxy Linux",
+    description : "EasyProxy helps to setup proxy in Linux OS in just one command",
+    github_link : "https://github.com/sushruta19/easy-proxy",
+    live_link : "",
+    imgUrl : ""
+  }
+];
+export default projects;
