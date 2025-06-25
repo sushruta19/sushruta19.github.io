87201194c81e62b41c240cb6ef0da0c6ade55d1d
Soubhik Sen <sensoubhik2001@gmail.com>
2025-06-25 08:54:18 +0530

    Update projects.js

--- src/content/projects.js
+++ src/content/projects.js
@@ -1,6 +1,22 @@
 const projects = [
   {
     id : 1,
+    name : "Book Scraper",
+    description: "A Robust Scrapy Crawler with Proxies, User-Agent Rotation, and MySQL Integration",
+    github_link: "https://github.com/sushruta19/bookscraper",
+    live_link: "",
+    imgUrl: ""
+  },
+  {
+    id : 2,
+    name : "Bike Sales Dashboard",
+    description: "A Bike Sales Data Cleaning and Dashboard in MS Excel",
+    github_link: "https://github.com/sushruta19/bike-sales-dashboard",
+    live_link: "",
+    imgUrl: "/assets/dashboard.png"
+  },
+  {
+    id : 3,
     name : "Book Store",
     description: "Full stack MERN project that allows users to display, add, edit, and delete books",
     github_link: "https://github.com/sushruta19/book-store",
@@ -8,28 +24,20 @@ const projects = [
     imgUrl: "/assets/book-store-card-view.png"
   },
   {
-    id : 2,
-    name : "Dummy Crypto Coin",
+    id : 4,
+    name : "Simplified Blockchain",
     description: "A simplified version of bitcoin blockchain implemented from scratch in TypeScript",
     github_link: "https://github.com/sushruta19/dummycoin",
     live_link: "",
     imgUrl: ""
   },
   {
-    id : 3,
+    id : 5,
     name : "EJS Blog Application",
     description: "A simple blog website created using EJS templates and Express with MongoDB database integration",
     github_link: "https://github.com/sushruta19/beginner-blog-application",
     live_link: "",
     imgUrl: "/assets/ejs-blog-page.png"
-  },
-  {
-    id : 4,
-    name : "EasyProxy Linux",
-    description : "EasyProxy helps to setup proxy in Linux OS in just one command",
-    github_link : "https://github.com/sushruta19/easy-proxy",
-    live_link : "",
-    imgUrl : ""
   }
 ];
 export default projects;
